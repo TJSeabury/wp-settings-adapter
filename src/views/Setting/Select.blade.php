e58f6b4c6e35038f0af6f4d249af47cb86ba809a
Tyler Seabury <tylerseabury@gmail.com>
2022-10-05 09:41:28 -0400

    added select view template

---
+++ src/views/Setting/Select.blade.php
@@ -0,0 +1,9 @@
+<div class="select-field">
+  <select name="" id="">
+    @foreach ($values as $value)
+      <option value="{{ $value }}">
+        {{ str_replace }}
+      </option>
+    @endforeach
+  </select>
+</div>
