bf5d517eb063200d1dcb854076a455be574e2a80
Tyler Seabury <tylerseabury@gmail.com>
2022-10-05 16:50:51 -0400

    created SubPageCollection

---
+++ src/models/SubPageCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ArdentIntent\WpSettingsAdapter\models;
+
+use ArdentIntent\WpSettingsAdapter\facades\SubPage;
+
+class SubPageCollection extends AbstractTypedArray
+{
+  const ARRAY_TYPE = SubPage::class;
+}
